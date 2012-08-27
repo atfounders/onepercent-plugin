1bc5a7d993dbd98cc3a50ff2917e03e4cd15e8d0
Ryan Imel <ryanimel@Ryan-Imels-MacBook-Air.local>
2012-08-27 12:45:24 -0400

    Initialized the plugin file.

---
+++ onepercent-functionality.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+Plugin Name: One Percent Pledge Site Functionality
+Description: Provides non-theme functionality to the One Percent Pledge site on the Founders network of sites.
+*/
+
+
+
+
