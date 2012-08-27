eec0f4e6d72b97b139e57287923c5213ecf656a7
Ryan Imel <ryanimel@Ryan-Imels-MacBook-Air.local>
2012-08-27 12:49:56 -0400

    Added the company post type.

--- onepercent-functionality.php
+++ onepercent-functionality.php
@@ -5,6 +5,16 @@ Plugin Name: One Percent Pledge Site Functionality
 Description: Provides non-theme functionality to the One Percent Pledge site on the Founders network of sites.
 */
 
+// Register the post types we need.
+add_action( 'init', 'rwi_onepercent_post_types' );
 
+function rwi_onepercent_post_types() {
+	
+	// We just need basic arguments to begin with.
+	$onepercent_company_args = array( 'public' => true, 'label' => 'Companies' );
+	
+	register_post_type( 'onepercent_company', $onepercent_company_args );
+	
+}
 
 
