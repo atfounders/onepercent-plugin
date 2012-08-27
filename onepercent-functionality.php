a86bb5fb3e7953b104c7467d9809b7e7a6991987
Ryan Imel <ryanimel@Ryan-Imels-MacBook-Air.local>
2012-08-27 12:55:29 -0400

    Added to companies post type a bit. Also removed link manager and pages post type from the dashboard nav.

--- onepercent-functionality.php
+++ onepercent-functionality.php
@@ -11,10 +11,26 @@ add_action( 'init', 'rwi_onepercent_post_types' );
 function rwi_onepercent_post_types() {
 	
 	// We just need basic arguments to begin with.
-	$onepercent_company_args = array( 'public' => true, 'label' => 'Companies' );
+	$onepercent_company_args = array( 
+		'exclude_from_search' => true,
+		'label' => 'Companies',
+		// The primary use of this site is to list these companies off. So go ahead and put it above Posts, in this case.
+		'menu_position' => 3,
+		'public' => true,
+		'show_in_menu_bar' => false
+	);
 	
 	register_post_type( 'onepercent_company', $onepercent_company_args );
 	
 }
 
 
+// Remove menu items that we don't need. This is a fairly simple WordPress site.
+add_action( 'admin_menu', 'rwi_onepercent_dashboard_simplify' );
+
+function rwi_onepercent_dashboard_simplify() {
+	
+	remove_menu_page( 'link-manager.php' );
+	remove_menu_page( 'edit.php?post_type=page');
+	
+}
